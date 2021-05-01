d1ffce5d79b736710953fd2cac873ab80f251b5d
cosmobowt <anishsp21@gmail.com>
2021-05-01 18:57:52 +0530

    Enter

---
+++ ZONe Energy Programming Contest/A/index.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string pres = "ZONe";
+    int count = 0;
+    string s;
+    cin >> s;
+
+    int found = s.find(pres, 0);
+    int ssize = s.size();
+
+    while (found != string::npos)
+    {
+        count++;
+        found = s.find(pres, found + 1);
+    }
+
+    cout << count;
+}
