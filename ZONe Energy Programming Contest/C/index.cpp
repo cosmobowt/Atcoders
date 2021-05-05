d29f9e88dd34674d1c7d72f1d5527bc8ae5a5c91
cosmobowt <anishsp21@gmail.com>
2021-05-05 20:00:28 +0530

    Enter

---
+++ ZONe Energy Programming Contest/C/index.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int con = 5;
+    int con2 = 5;
+    vector<int> v;
+    vector<int> v;
+
+    while (n--)
+    {
+        while (con--)
+        {
+            int value;
+            cin >> value;
+            v.push_back(value)
+        }
+    }
+
+    while (con2--)
+    {
+        v.
+    }
+}
