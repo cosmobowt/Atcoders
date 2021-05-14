206efda485eb79a40ef0991b85f53a96f85a1099
cosmobowt <anishsp21@gmail.com>
2021-05-14 23:27:26 +0530

    Enter

---
+++ KYOCERA Programming Contest 2021（AtCoder Beginner Contest 200）/B - 200th ABC-200/index.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n, k;
+    cin >> n >> k;
+
+    while (k--)
+    {
+        if (n % 200 == 0)
+        {
+            n /= 200;
+            // cout << n << endl;
+        }
+        else
+        {
+            n *= 1000;
+            n += 200;
+            // cout << n << endl;
+        }
+    }
+
+    cout << n;
+}
