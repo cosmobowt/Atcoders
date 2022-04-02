4a591647ac1ea91207951b9b5dc9d75b65bc4a6a
cosmobowt <anishsp21@gmail.com>
2022-04-02 17:51:51 +0530

    commit

---
+++ AtCoder Beginner Contest 246/A - Four Points/index.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define fu(i, n) for (int i = 1; i <= n; i++)
+#define MOD 1000000007
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> P;
+
+int main()
+{
+}
